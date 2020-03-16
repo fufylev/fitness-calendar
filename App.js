6d0ecdcd165e88aad3ee8d77347fc926611e0018
Andrey <35691656+fufylev@users.noreply.github.com>
2020-03-16 10:38:49 +0300

    The main App's structures has been prepared

--- App.js
+++ App.js
@@ -1,19 +1,115 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Button, Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import IconWithBadge from "./components/IconWithBadge";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+function HomeIconWithBadge(props) {
+    // You should pass down the badgeCount in some other ways like React Context API, Redux, MobX or event emitters.
+    return <IconWithBadge {...props} badgeCount={0}/>;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function History({navigation}) {
+    return (
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <Text>History screen</Text>
+        </View>
+    );
+}
+
+function AddEntry({navigation}) {
+    return (
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <Text>Settings screen</Text>
+        </View>
+    );
+}
+
+const HomeStack = createStackNavigator();
+
+function HomeStackScreen() {
+    return (
+        <HomeStack.Navigator>
+            <HomeStack.Screen name="Home" component={History} options={stackOptions.home}/>
+        </HomeStack.Navigator>
+    );
+}
+
+const AddEntryStack = createStackNavigator();
+
+function AddEntryStackScreen() {
+    return (
+        <AddEntryStack.Navigator>
+            <AddEntryStack.Screen name="Settings" component={AddEntry} options={stackOptions.setting}/>
+        </AddEntryStack.Navigator>
+    );
+}
+
+const Tab = createBottomTabNavigator();
+
+const stackOptions = {
+    home: {
+        headerTitle: 'History',
+        headerRight: () => (
+            <Button
+                onPress={() => alert('This is a button!')}
+                title="Info"
+                // color="#fff"
+            />
+        ),
+    },
+    setting: {
+        headerTitle: 'Add Entry',
+        headerRight: () => (
+            <Button
+                onPress={() => alert('This is a button!')}
+                title="Info"
+                // color="#fff"
+            />
+        ),
+    }
+};
+
+export default function App() {
+    return (
+        <NavigationContainer>
+            <Tab.Navigator
+                screenOptions={({route}) => ({
+                    tabBarIcon: ({focused, color, size}) => {
+                        if (route.name === 'History') {
+                            return (
+                                <HomeIconWithBadge
+                                    name={
+                                        focused
+                                            ? 'ios-information-circle'
+                                            : 'ios-information-circle-outline'
+                                    }
+                                    size={size}
+                                    color={color}
+                                />
+                            );
+                        } else if (route.name === 'Add Entry') {
+                            return (
+                                <Ionicons
+                                    name={focused ? 'ios-list-box' : 'ios-list'}
+                                    size={size}
+                                    color={color}
+                                />
+                            );
+                        }
+                    },
+                })}
+                tabBarOptions={{
+                    activeTintColor: 'tomato',
+                    inactiveTintColor: 'gray',
+                }}
+            >
+                <Tab.Screen name="History" component={HomeStackScreen}/>
+                <Tab.Screen name="Add Entry" component={AddEntryStackScreen}/>
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
+}
