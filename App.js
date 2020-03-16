9c0a2564f95d4904532a5031688f03a9a685530a
Andrey <35691656+fufylev@users.noreply.github.com>
2020-03-16 21:03:08 +0300

    Location and Permissions were added and applied
    Location and Permissions were added and applied

--- App.js
+++ App.js
@@ -1,146 +1,154 @@
-import 'react-native-gesture-handler';
-import React from 'react';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Button } from 'react-native';
-import { FontAwesome, Ionicons } from '@expo/vector-icons';
+import "react-native-gesture-handler";
+import React from "react";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Button } from "react-native";
+import { FontAwesome, Ionicons } from "@expo/vector-icons";
 import IconWithBadge from "./components/IconWithBadge";
-import reducer from './reducers';
-import History from './components/History';
-import AddEntry from './components/AddEntry';
+import reducer from "./reducers";
+import History from "./components/History";
+import AddEntry from "./components/AddEntry";
 import EntryDetails from "./components/EntryDetails";
 import Live from "./components/Live";
-import { purple, white } from './utils/colors';
+import { purple, white } from "./utils/colors";
 
 function HomeIconWithBadge(props) {
-    return <IconWithBadge {...props} badgeCount={0}/>;
+  return <IconWithBadge {...props} badgeCount={0} />;
 }
 
 const HomeStack = createStackNavigator();
 
 function HomeStackScreen(props) {
-    return (
-        <HomeStack.Navigator>
-            <HomeStack.Screen name="Home" component={History} options={stackOptions.home}/>
-            <HomeStack.Screen
-                name="Entry Details"
-                component={EntryDetails}
-                options={({ route }) => ({ title: route.params.name })}
-            />
-        </HomeStack.Navigator>
-    );
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen
+        name='Home'
+        component={History}
+        options={stackOptions.home}
+      />
+      <HomeStack.Screen
+        name='Entry Details'
+        component={EntryDetails}
+        options={({ route }) => ({ title: route.params.name })}
+      />
+    </HomeStack.Navigator>
+  );
 }
 
 const AddEntryStack = createStackNavigator();
 
 function AddEntryStackScreen() {
-    return (
-        <AddEntryStack.Navigator>
-            <AddEntryStack.Screen name="Settings" component={AddEntry} options={stackOptions.setting}/>
-        </AddEntryStack.Navigator>
-    );
+  return (
+    <AddEntryStack.Navigator>
+      <AddEntryStack.Screen
+        name='Settings'
+        component={AddEntry}
+        options={stackOptions.setting}
+      />
+    </AddEntryStack.Navigator>
+  );
 }
 
 const LiveStack = createStackNavigator();
 
 function LiveStackScreen() {
-    return (
-        <LiveStack.Navigator>
-            <LiveStack.Screen name="Settings" component={Live} options={stackOptions.live}/>
-        </LiveStack.Navigator>
-    );
+  return (
+    <LiveStack.Navigator>
+      <LiveStack.Screen
+        name='Settings'
+        component={Live}
+        options={stackOptions.live}
+      />
+    </LiveStack.Navigator>
+  );
 }
 
 const Tab = createBottomTabNavigator();
 
 const stackOptions = {
-    home: {
-        headerTitle: 'History',
-        headerRight: () => (
-            <Button
-                onPress={() => alert('This is a button!')}
-                title="Info"
-                // color="#fff"
-            />
-        ),
-    },
-    setting: {
-        headerTitle: 'Add Entry',
-        headerRight: () => (
-            <Button
-                onPress={() => alert('This is a button!')}
-                title="Info"
-                // color="#fff"
-            />
-        ),
-    },
-    live: {
-        headerTitle: 'Live',
-    }
+  home: {
+    headerTitle: "History",
+    headerRight: () => (
+      <Button
+        onPress={() => alert("This is a button!")}
+        title='Info'
+        // color="#fff"
+      />
+    ),
+  },
+  setting: {
+    headerTitle: "Add Entry",
+    headerRight: () => (
+      <Button
+        onPress={() => alert("This is a button!")}
+        title='Info'
+        // color="#fff"
+      />
+    ),
+  },
+  live: {
+    headerTitle: "Live",
+  },
 };
 
 export default function App() {
-    return (
-        <Provider store={createStore(reducer)}>
-            <NavigationContainer>
-                <Tab.Navigator
-                    screenOptions={({route}) => ({
-                        tabBarIcon: ({focused, color, size = 30}) => {
-                            if (route.name === 'History') {
-                                return (
-                                    <HomeIconWithBadge
-                                        name={
-                                            focused
-                                                ? 'view-dashboard'
-                                                : 'view-dashboard-outline'
-                                        }
-                                        size={30}
-                                        color={color}
-                                    />
-                                );
-                            } else if (route.name === 'Add Entry') {
-                                return (
-                                    <FontAwesome
-                                        name={focused ? 'plus-square' : 'plus-square-o'}
-                                        size={30}
-                                        color={color}
-                                    />
-                                );
-                            } else if (route.name === 'Live') {
-                                return (
-                                    <Ionicons
-                                        name={focused ? 'ios-speedometer' : 'md-speedometer'}
-                                        size={30}
-                                        color={color}
-                                    />
-                                );
-                            }
-                        },
-                    })}
-                    tabBarOptions={{
-                        activeTintColor: Platform.OS === 'ios' ? purple : white,
-                        inactiveTintColor: 'gray',
-                        style: {
-                            // height: 56,
-                            backgroundColor: Platform.OS === 'ios' ? white : purple,
-                            shadowColor: 'rgba(0, 0, 0, 0.24)',
-                            shadowOffset: {
-                                width: 0,
-                                height: 3
-                            },
-                            shadowRadius: 6,
-                            shadowOpacity: 1
-                        },
-                    }}
-                >
-                    <Tab.Screen name="History" component={HomeStackScreen}/>
-                    <Tab.Screen name="Add Entry" component={AddEntryStackScreen}/>
-                    <Tab.Screen name="Live" component={LiveStackScreen}/>
-                </Tab.Navigator>
-            </NavigationContainer>
-        </Provider>
-    );
+  return (
+    <Provider store={createStore(reducer)}>
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size = 30 }) => {
+              if (route.name === "History") {
+                return (
+                  <HomeIconWithBadge
+                    name={focused ? "view-dashboard" : "view-dashboard-outline"}
+                    size={30}
+                    color={color}
+                  />
+                );
+              } else if (route.name === "Add Entry") {
+                return (
+                  <FontAwesome
+                    name={focused ? "plus-square" : "plus-square-o"}
+                    size={30}
+                    color={color}
+                  />
+                );
+              } else if (route.name === "Live") {
+                return (
+                  <Ionicons
+                    name={focused ? "ios-speedometer" : "md-speedometer"}
+                    size={30}
+                    color={color}
+                  />
+                );
+              }
+            },
+          })}
+          tabBarOptions={{
+            activeTintColor: Platform.OS === "ios" ? purple : white,
+            inactiveTintColor: "gray",
+            style: {
+              // height: 56,
+              backgroundColor: Platform.OS === "ios" ? white : purple,
+              shadowColor: "rgba(0, 0, 0, 0.24)",
+              shadowOffset: {
+                width: 0,
+                height: 3,
+              },
+              shadowRadius: 6,
+              shadowOpacity: 1,
+            },
+          }}
+        >
+          <Tab.Screen name='History' component={HomeStackScreen} />
+          <Tab.Screen name='Add Entry' component={AddEntryStackScreen} />
+          <Tab.Screen name='Live' component={LiveStackScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </Provider>
+  );
 }
--- components/Live.js
+++ components/Live.js
@@ -1,78 +1,108 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ActivityIndicator, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import * as Location from 'expo-location';
 import * as Permissions from 'expo-permissions';
 import { Foundation } from '@expo/vector-icons';
 import { purple, red, white } from '../utils/colors';
+import { calculateDirection } from '../utils/helpers';
 
 const Live = () => {
     const [coords, setCoords] = useState(null);
-    const [status, setStatus] = useState('granted');
+    const [status, setStatus] = useState(null);
     const [direction, setDirection] = useState('');
 
-    const askPermission = () => {
+    useEffect(() => {
+        Permissions.getAsync(Permissions.LOCATION)
+            .then(({ status }) => {
+                if (status === 'granted') {
+                    return setLocation();
+                }
+                setStatus(status);
+            })
+            .catch(error => {
+                console.warn('Error getting Location permission: ', error);
 
+                setStatus('undetermined');
+            });
+    }, []);
+
+    const askPermission = () => {
+        Permissions.askAsync(Permissions.LOCATION)
+            .then(({ status }) => {
+                if (status === 'granted') {
+                    return setLocation();
+                }
+
+                setStatus(status);
+            })
+            .catch(error => console.warn('error asking Location permission', error));
+    };
+
+    const setLocation = () => {
+        Location.watchPositionAsync(
+            {
+                enableHighAccuracy: true,
+                timeInterval: 1,
+                distanceInterval: 1,
+            },
+            ({ coords }) => {
+                const newDirection = calculateDirection(coords.heading);
+
+                setCoords(coords);
+                setDirection(newDirection);
+                setStatus('granted');
+            },
+        );
     };
 
     if (status === null) {
-        return <ActivityIndicator style={{marginTop: 30}}/>
+        return <ActivityIndicator style={{ marginTop: 30 }} />;
     }
 
     if (status === 'denied') {
         return (
             <View style={styles.center}>
-                <Foundation name='alert' size={50} color={red}/>
+                <Foundation name="alert" size={50} color={red} />
                 <Text>
-                    You denied your location. You can fix this by visiting your settings and enabling location services for this app.
+                    You denied your location. You can fix this by visiting your settings and enabling location services
+                    for this app.
                 </Text>
             </View>
-        )
+        );
     }
 
     if (status === 'undetermined') {
         return (
             <View style={styles.center}>
-                <Foundation name='alert' size={50} />
-                <Text>
-                    You need to enable location services for this app.
-                </Text>
-                <TouchableOpacity style={styles.button} onPress={this.askPermission}>
-                    <Text style={styles.buttonText}>
-                        Enable
-                    </Text>
+                <Foundation name="alert" size={50} />
+                <Text>You need to enable location services for this app.</Text>
+                <TouchableOpacity style={styles.button} onPress={askPermission}>
+                    <Text style={styles.buttonText}>Enable</Text>
                 </TouchableOpacity>
             </View>
-        )
+        );
     }
 
     return (
         <View style={styles.container}>
             <View style={styles.directionContainer}>
                 <Text style={styles.header}>You're heading</Text>
-                <Text style={styles.direction}>
-                    North
-                </Text>
+                <Text style={styles.direction}>{direction}</Text>
             </View>
             <View style={styles.metricContainer}>
                 <View style={styles.metric}>
-                    <Text style={[styles.header, {color: white}]}>
-                        Altitude
-                    </Text>
-                    <Text style={[styles.subHeader, {color: white}]}>
-                        {200} feet
+                    <Text style={[styles.header, { color: white }]}>Altitude</Text>
+                    <Text style={[styles.subHeader, { color: white }]}>
+                        {(coords.altitude * 3.2808).toFixed(1)} Feet
                     </Text>
                 </View>
                 <View style={styles.metric}>
-                    <Text style={[styles.header, {color: white}]}>
-                        Speed
-                    </Text>
-                    <Text style={[styles.subHeader, {color: white}]}>
-                        {300} MPH
-                    </Text>
+                    <Text style={[styles.header, { color: white }]}>Speed</Text>
+                    <Text style={[styles.subHeader, { color: white }]}>{(coords.speed * 2.2369).toFixed(1)} MPH</Text>
                 </View>
             </View>
         </View>
-    )
+    );
 };
 
 Live.propTypes = {};
@@ -80,7 +110,7 @@ Live.propTypes = {};
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'space-between'
+        justifyContent: 'space-between',
     },
     center: {
         flex: 1,
@@ -96,7 +126,7 @@ const styles = StyleSheet.create({
         borderRadius: 5,
         margin: 20,
     },
-    buttonText :{
+    buttonText: {
         color: white,
         fontSize: 20,
     },
